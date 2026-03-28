ac8d6e7bf5d25137debbafb1a13f3abd4c2377cc
yuki yorita <example@co.com>
2026-03-28 21:29:07 +0900

    templates追加

---
+++ templates/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        // テストケースをここに書く
+    }
+
+    // 解法をここに書く
+    public void solve() {
+
+    }
+}
